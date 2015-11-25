65d630058f9bad5fa15393bf1c2509965f688127
Dmitry Plechystyy <only.one.trogvar@gmail.com>
2015-11-25 17:44:42 +0200

    Draft of the start screen UI added

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("LevelManager::LoadLevel() called for " + name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("LevelManager::QuitRequest() called");
+	}
+}
